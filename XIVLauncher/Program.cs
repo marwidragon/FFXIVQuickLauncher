dfc21dccbc21ceca7916a0ce549dfc4dc54445fe
anoyetta <anoyetta@gmail.com>
2019-02-07 19:58:32 +0900

    Optionを開こうとすると異常終了する不具合を修正した

--- XIVLauncher/Program.cs
+++ XIVLauncher/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using System.Net;
+using System.Text;
 using System.Windows.Forms;
 using XIVLauncher.WPF.Models;
 using XIVLauncher.WPF.Views;
@@ -28,6 +30,22 @@ namespace XIVLauncher
                 var app = new System.Windows.Application()
                 {
                     ShutdownMode = System.Windows.ShutdownMode.OnMainWindowClose
+                };
+
+                app.DispatcherUnhandledException += (_, e) =>
+                {
+                    Settings.Instance.Save();
+
+                    var message = string.Empty;
+                    message += "予期しない例外が発生しました。アプリケーションを終了します。\n\n";
+                    message += e.Exception.ToString() + "\n";
+
+                    File.WriteAllText(
+                        "error.log",
+                        message,
+                        new UTF8Encoding(false));
+
+                    MessageBox.Show(message, "Fatal");
                 };
 
                 app.Exit += (x, y) => Settings.Instance.Save();
